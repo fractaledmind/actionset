379f39fde7dbc0591f0581f0b8c51611b6daba09
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-10-04 13:34:15 -0400

    Reorganize the request specs

---
+++ spec/requests/sort/computed_fields/symbol_spec.rb
@@ -0,0 +1,144 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'GET /foos with SORTING', type: :request do
+  let!(:foo) { FactoryGirl.create(:foo) }
+  let!(:others) { FactoryGirl.create_list(:foo, 2) }
+  let(:results) { JSON.parse(response.body) }
+  let(:results_ids) { results.map { |f| f['id'] } }
+
+  before(:each) do
+    get foos_path, params: params, headers: {}
+  end
+
+  context 'with a :calculated_symbol_field attribute' do
+    context 'on the base object' do
+      let(:params) do
+        { sort: sort_param }
+      end
+
+      context 'when direction is ASC' do
+        let(:direction) { :asc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by(&:calculated_symbol_field)
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field(i)': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by { |x| x.calculated_symbol_field.to_s.downcase }
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+
+      context 'when direction is DESC' do
+        let(:direction) { :desc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by(&:calculated_symbol_field)
+               .reverse
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field(i)': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by { |x| x.calculated_symbol_field.to_s.downcase }
+               .reverse
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+    end
+
+    context 'on an associated object' do
+      let(:params) do
+        { sort: { assoc: sort_param } }
+      end
+
+      context 'when direction is ASC' do
+        let(:direction) { :asc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by { |x| x.assoc.calculated_symbol_field }
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field(i)': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by { |x| x.assoc.calculated_symbol_field.to_s.downcase }
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+
+      context 'when direction is DESC' do
+        let(:direction) { :desc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by { |x| x.assoc.calculated_symbol_field }
+               .reverse
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'calculated_symbol_field(i)': direction } }
+          let(:expected_ids) do
+            Foo.all
+               .sort_by { |x| x.assoc.calculated_symbol_field.to_s.downcase }
+               .reverse
+               .map(&:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+    end
+  end
+end
---
+++ spec/requests/sort/db_fields/text_spec.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'GET /foos with SORTING', type: :request do
+  let!(:foo) { FactoryGirl.create(:foo) }
+  let!(:others) { FactoryGirl.create_list(:foo, 2) }
+  let(:results) { JSON.parse(response.body) }
+  let(:results_ids) { results.map { |f| f['id'] } }
+
+  before(:each) do
+    get foos_path, params: params, headers: {}
+  end
+
+  context 'with a :text attribute type' do
+    context 'on the base object' do
+      let(:params) do
+        { sort: sort_param }
+      end
+
+      context 'when direction is ASC' do
+        let(:direction) { :asc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'text': direction } }
+          let(:expected_ids) do
+            Foo.order(text: direction)
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'text(i)': direction } }
+          let(:expected_ids) do
+            Foo.order("LOWER(text) #{direction}")
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+
+      context 'when direction is DESC' do
+        let(:direction) { :desc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'text': direction } }
+          let(:expected_ids) do
+            Foo.order(text: direction)
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'text(i)': direction } }
+          let(:expected_ids) do
+            Foo.order("LOWER(text) #{direction}")
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+    end
+
+    context 'on an associated object' do
+      let(:params) do
+        { sort: { assoc: sort_param } }
+      end
+
+      context 'when direction is ASC' do
+        let(:direction) { :asc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'text': direction } }
+          let(:expected_ids) do
+            Foo.joins(:assoc)
+               .merge(Assoc.order(text: direction))
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'text(i)': direction } }
+          let(:expected_ids) do
+            Foo.joins(:assoc)
+               .merge(Assoc.order("LOWER(#{Assoc.table_name}.text) #{direction}"))
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+
+      context 'when direction is DESC' do
+        let(:direction) { :desc }
+
+        context 'with default case-sensitive sort' do
+          let(:sort_param) { { 'text': direction } }
+          let(:expected_ids) do
+            Foo.joins(:assoc)
+               .merge(Assoc.order(text: direction))
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+
+        context 'with case-INsensitive sort' do
+          let(:sort_param) { { 'text(i)': direction } }
+          let(:expected_ids) do
+            Foo.joins(:assoc)
+               .merge(Assoc.order("LOWER(#{Assoc.table_name}.text) #{direction}"))
+               .pluck(:id)
+          end
+
+          it { expect(response).to have_http_status :ok }
+          it { expect(results_ids).to eq expected_ids }
+        end
+      end
+    end
+  end
+end
