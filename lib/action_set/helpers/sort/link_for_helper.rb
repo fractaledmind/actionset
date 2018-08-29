33a5bb14cc8f79b367e2ab6c0da70f1c4e08e57b
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-08-29 15:54:26 -0500

    Ensure that the sort_link_for helper method can work with symbol attributes

--- lib/action_set/helpers/sort/link_for_helper.rb
+++ lib/action_set/helpers/sort/link_for_helper.rb
@@ -3,7 +3,7 @@
 module Sort
   module LinkForHelper
     def sort_link_for(attribute, name = nil)
-      link_to(name || attribute.titleize,
+      link_to(name || attribute.to_s.titleize,
               sort_path_for(attribute),
               'aria-label': sort_description_for(attribute))
     end
