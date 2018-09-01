65e97b8f57a561290dc94ba89c1813cd64d55cb1
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-09-01 14:50:00 -0500

    Update the HelperMethods module to bring in the primary helpers

--- lib/action_set/helpers/helper_methods.rb
+++ lib/action_set/helpers/helper_methods.rb
@@ -1,24 +1,16 @@
 # frozen_string_literal: true
 
-require_relative './sort/current_direction_for_helper'
-require_relative './sort/description_for_helper'
 require_relative './sort/link_for_helper'
-require_relative './sort/next_direction_for_helper'
-require_relative './sort/path_for_helper'
 require_relative './pagination/links_for_helper'
 require_relative './pagination/path_for_helper'
+require_relative './params/form_for_object_helper'
 
 module ActionSet
   module Helpers
     module HelperMethods
-      include Sort::CurrentDirectionForHelper
-      include Sort::DescriptionForHelper
       include Sort::LinkForHelper
-      include Sort::NextDirectionForHelper
-      include Sort::PathForHelper
-
-      include Pagination::PathForHelper
       include Pagination::LinksForHelper
+      include Params::FormForObjectHelper
     end
   end
 end
