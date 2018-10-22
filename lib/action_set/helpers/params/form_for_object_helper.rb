eef16619daa6358ffcadc932a98e0761bac638d3
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-10-22 17:21:05 -0400

    Allow the form_for_object_from_param helper to accept defaults hash

--- lib/action_set/helpers/params/form_for_object_helper.rb
+++ lib/action_set/helpers/params/form_for_object_helper.rb
@@ -5,11 +5,12 @@ require 'ostruct'
 
 module Params
   module FormForObjectHelper
-    def form_for_object_from_param(param)
-      form_for_params = params.fetch(param, {})
+    def form_for_object_from_param(param, defaults = {})
+      form_for_params = current_params.fetch(param, {})
       form_for_requirements = { model_name: { param_key: param } }
+      form_for_hash = defaults.merge(form_for_params).merge(form_for_requirements)
 
-      JSON.parse(form_for_params.merge(form_for_requirements).to_json,
+      JSON.parse(form_for_hash.to_json,
                  object_class: OpenStruct)
     end
   end
