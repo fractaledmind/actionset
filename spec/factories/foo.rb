4dd40976b21a3f342636b4fd30074919950aa1aa
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-11-12 09:45:24 -0500

    Update the factories to better ensure no duplicate attributes

--- spec/factories/foo.rb
+++ spec/factories/foo.rb
@@ -1,24 +1,17 @@
 # frozen_string_literal: true
 
-  sequence :string do |n|
-    [Faker::RickAndMorty.character, n].join('-')
-  end
-  sequence :text do |n|
-    [Faker::RickAndMorty.quote, n].join('-')
-  end
-
 FactoryBot.define do
   factory :foo do
-    binary        { Base64.encode64 Faker::Crypto.sha256 }
-    boolean       { [true, false].sample }
-    date          { Faker::Date.between(1.year.ago, 1.year.from_now).to_date }
-    datetime      { Faker::Time.between(1.year.ago, 1.year.from_now).to_datetime }
-    decimal       { Faker::Number.decimal(2).to_f }
-    float         { Faker::Number.decimal(2).to_f }
-    integer       { Faker::Number.between(1, 100) }
-    string        { generate(:string) }
-    text          { generate(:text) }
-    time          { Faker::Time.between(1.year.ago, 1.year.from_now).to_s[12..-1] }
+    sequence(:binary) { |n| Base64.encode64 [Faker::Crypto.sha256, n].join('-') }
+    sequence(:boolean, &:even?)
+    sequence(:date)     { |n| Faker::Date.between(n.send(:year).ago, n.send(:year).from_now).to_date }
+    sequence(:datetime) { |n| Faker::Date.between(n.send(:year).ago, n.send(:year).from_now).to_datetime }
+    sequence(:decimal)  { |n| Faker::Number.decimal(2).to_f + n }
+    sequence(:float)    { |n| Faker::Number.decimal(2).to_f + n }
+    sequence(:integer, &:to_i)
+    sequence(:string)   { |n| [Faker::RickAndMorty.character, n].join('-') }
+    sequence(:text)     { |n| [Faker::RickAndMorty.quote[0..50], n].join('-') }
+    sequence(:time)     { |n| Faker::Time.between(n.send(:year).ago, n.send(:year).from_now).to_s[12..-1] }
 
     association :assoc
   end
