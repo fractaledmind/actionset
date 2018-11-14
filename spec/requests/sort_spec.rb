fbafcfac96c4ceaf9a7aae88d98b088e0e72ae7d
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-11-14 15:56:20 -0500

    Get the sorting request specs to be programmatically generated

--- spec/requests/sort_spec.rb
+++ spec/requests/sort_spec.rb
@@ -2,81 +2,87 @@
 
 require 'spec_helper'
 
-RSpec.describe 'GET /foos with SORTING', type: :request do
-  let!(:foo) { FactoryGirl.create(:foo) }
-  let!(:others) { FactoryGirl.create_list(:foo, 2) }
-  let(:results) { JSON.parse(response.body) }
-  let(:results_ids) { results.map { |f| f['id'] } }
+RSpec.describe 'GET /foos?sort', type: :request do
+  before(:all) do
+    @foo_1 = FactoryBot.create(:foo, string: 'a', integer: 1, boolean: true,
+                                     date: 1.day.from_now.to_date, datetime: 1.day.from_now.to_datetime,
+                                     decimal: 1.1, float: 1.1, time: 1.hour.from_now.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'ra', integer: 9))
+    @foo_2 = FactoryBot.create(:foo, string: 'a', integer: 2, boolean: true,
+                                     date: 1.day.ago.to_date, datetime: 1.day.ago.to_datetime,
+                                     decimal: 2.2, float: 2.2, time: 2.hours.from_now.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'ra', integer: 8))
+    @foo_3 = FactoryBot.create(:foo, string: 'z', integer: 1, boolean: true,
+                                     date: 1.day.from_now.to_date, datetime: 1.day.from_now.to_datetime,
+                                     decimal: 1.1, float: 1.1, time: 1.hour.ago.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'rz', integer: 9))
+    @foo_4 = FactoryBot.create(:foo, string: 'z', integer: 2, boolean: true,
+                                     date: 1.day.ago.to_date, datetime: 1.day.ago.to_datetime,
+                                     decimal: 2.2, float: 2.2, time: 2.hours.ago.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'rz', integer: 8))
+    @foo_5 = FactoryBot.create(:foo, string: 'A', integer: 1, boolean: false,
+                                     date: 1.week.from_now.to_date, datetime: 1.week.from_now.to_datetime,
+                                     decimal: 1.1, float: 1.1, time: 1.hour.from_now.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'rA', integer: 9))
+    @foo_6 = FactoryBot.create(:foo, string: 'A', integer: 2, boolean: false,
+                                     date: 1.week.ago.to_date, datetime: 1.week.ago.to_datetime,
+                                     decimal: 2.2, float: 2.2, time: 2.hours.from_now.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'rA', integer: 8))
+    @foo_7 = FactoryBot.create(:foo, string: 'Z', integer: 1, boolean: false,
+                                     date: 1.week.from_now.to_date, datetime: 1.week.from_now.to_datetime,
+                                     decimal: 1.1, float: 1.1, time: 1.hour.ago.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'rZ', integer: 9))
+    @foo_8 = FactoryBot.create(:foo, string: 'Z', integer: 2, boolean: false,
+                                     date: 1.week.ago.to_date, datetime: 1.week.ago.to_datetime,
+                                     decimal: 2.2, float: 2.2, time: 2.hours.ago.to_time.to_s[12..-1],
+                                     assoc: FactoryBot.create(:assoc, string: 'rZ', integer: 8))
+    @active_set = ActiveSet.new(Foo.all)
+    @all_foos = Foo.all.to_a
+  end
+  after(:all) { Foo.delete_all }
 
-  context 'JSON request' do
+  def value_for(path:, object:)
+    value = path.split('.').reduce(object) { |obj, m| obj&.send('fetch', m.gsub('(i)', '')) }
+    return value.to_s if [true, false].include? value
+    return value.downcase if path.end_with? '(i)'
+
+    value
+  end
+
+  context '.json' do
+    let(:result) { JSON.parse(response.body) }
+
     before(:each) do
-      get foos_path(format: :json), params: params, headers: {}
+      get foos_path(format: :json), params: { sort: instructions }, headers: {}
     end
 
-    context 'db fields' do
-      ApplicationRecord::DB_FIELDS.each do |db_field|
-        context "with a :#{db_field} attribute type" do
-          context 'on the base object' do
-            let(:params) do
-              { sort: sort_param }
-            end
-
-            %w[asc desc].each do |direction|
-              context "when direction is #{direction.upcase}" do
-                context 'with default case-sensitive sort' do
-                  let(:sort_param) { { db_field => direction } }
-                  let(:expected_ids) do
-                    Foo.order(db_field => direction)
-                       .pluck(:id)
-                  end
-
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
+    ApplicationRecord::FIELD_TYPES.each do |type|
+      context "with #{type.upcase} type" do
+        [:asc, 'desc'].each do |direction|
+          context "in #{direction} direction" do
+            %W[
+              #{type}
+              computed_#{type}
+              assoc.#{type}
+              assoc.computed_#{type}
+              computed_assoc.#{type}
+              computed_assoc.computed_#{type}
+            ].each do |path|
+              context "{ #{path}: }" do
+                let(:instructions) do
+                  {
+                    path => direction
+                  }
                 end
 
-                context 'with case-INsensitive sort' do
-                  let(:sort_param) { { "#{db_field}(i)": direction } }
-                  let(:expected_ids) do
-                    Foo.order("LOWER(#{db_field}) #{direction}")
-                       .pluck(:id)
+                it do
+                  result.each_cons(2) do |left_result, right_result|
+                    left_value = value_for(path: path, object: left_result)
+                    right_value = value_for(path: path, object: right_result)
+                    operator = direction == :asc ? '<=' : '>='
+
+                    expect(left_value).to be.send(operator, right_value)
                   end
-
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
-                end
-              end
-            end
-          end
-
-          context 'on an associated object' do
-            let(:params) do
-              { sort: { assoc: sort_param } }
-            end
-
-            %w[asc desc].each do |direction|
-              context "when direction is #{direction.upcase}" do
-                context 'with default case-sensitive sort' do
-                  let(:sort_param) { { db_field => direction } }
-                  let(:expected_ids) do
-                    Foo.joins(:assoc)
-                       .merge(Assoc.order(db_field => direction))
-                       .pluck(:id)
-                  end
-
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
-                end
-
-                context 'with case-INsensitive sort' do
-                  let(:sort_param) { { "#{db_field}(i)": direction } }
-                  let(:expected_ids) do
-                    Foo.joins(:assoc)
-                       .merge(Assoc.order("LOWER(#{Assoc.table_name}.#{db_field}) #{direction}"))
-                       .pluck(:id)
-                  end
-
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
                 end
               end
             end
@@ -85,68 +91,141 @@ RSpec.describe 'GET /foos with SORTING', type: :request do
       end
     end
 
-    context 'computed fields' do
-      ApplicationRecord::COMPUTED_FIELDS.each do |computed_field|
-        context "with a :#{computed_field} attribute" do
-          let(:field) { "computed_#{computed_field}_field" }
-
-          context 'on the base object' do
-            let(:params) do
-              { sort: sort_param }
+    ApplicationRecord::FIELD_TYPES.combination(2).each do |type_1, type_2|
+      context "with #{type_1.upcase} and #{type_2.upcase} types" do
+        paths = %W[
+          #{type_1}
+          #{type_2}
+          computed_#{type_1}
+          computed_#{type_2}
+          assoc.#{type_1}
+          assoc.#{type_2}
+          assoc.computed_#{type_1}
+          assoc.computed_#{type_2}
+          computed_assoc.#{type_1}
+          computed_assoc.#{type_2}
+          computed_assoc.computed_#{type_1}
+          computed_assoc.computed_#{type_2}
+        ]
+        all_possible_instructions = paths
+                                    .combination(2)
+                                    .flat_map do |path_1, path_2|
+          [:asc, 'desc']
+            .repeated_permutation(2)
+            .map do |pair|
+              Hash[[path_1, path_2].zip(pair)]
             end
+        end
 
-            %w[asc desc].each do |direction|
-              context "when direction is #{direction.upcase}" do
-                context 'with default case-sensitive sort' do
-                  let(:sort_param) { { field => direction } }
-                  let(:expected_ids) do
-                    ActiveSet.new(Foo.all).sort(params[:sort]).map(&:id)
-                  end
+        all_possible_instructions.each do |instructions|
+          context "{ #{instructions} }" do
+            let(:instructions) { instructions }
 
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
-                end
+            it do
+              result.each_cons(2) do |left_result, right_result|
+                (path_1, path_2) = instructions.keys
+                (dir_1, dir_2) = instructions.values
 
-                context 'with case-INsensitive sort' do
-                  let(:sort_param) { { "#{field}(i)": direction } }
-                  let(:expected_ids) do
-                    ActiveSet.new(Foo.all).sort(params[:sort]).map(&:id)
-                  end
+                left_value_1 = value_for(path: path_1, object: left_result)
+                right_value_1 = value_for(path: path_1, object: right_result)
+                operator_1 = dir_1 == :asc ? '<=' : '>='
 
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
+                expect(left_value_1).to be.send(operator_1, right_value_1)
+
+                next unless left_value_1 == right_value_1
+
+                left_value_2 = value_for(path: path_2, object: left_result)
+                right_value_2 = value_for(path: path_2, object: right_result)
+                operator_2 = dir_2 == :asc ? '<=' : '>='
+
+                expect(left_value_2).to be.send(operator_2, right_value_2)
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context 'with STRING case-insensitive type' do
+      [:asc, 'desc'].each do |direction|
+        context "in #{direction} direction" do
+          %w[
+            string(i)
+            computed_string(i)
+            assoc.string(i)
+            assoc.computed_string(i)
+            computed_assoc.string(i)
+            computed_assoc.computed_string(i)
+          ].each do |path|
+            context "{ #{path}: }" do
+              let(:instructions) do
+                {
+                  path => direction
+                }
+              end
+
+              it do
+                result.each_cons(2) do |left_result, right_result|
+                  left_value = value_for(path: path, object: left_result)
+                  right_value = value_for(path: path, object: right_result)
+                  operator = direction == :asc ? '<=' : '>='
+
+                  expect(left_value).to be.send(operator, right_value)
                 end
               end
             end
           end
+        end
+      end
+    end
 
-          context 'on an associated object' do
-            let(:params) do
-              { sort: { assoc: sort_param } }
-            end
+    context 'with STRING case-insenstive and INTEGER types' do
+      paths = %w[
+        string(i)
+        integer
+        computed_string(i)
+        computed_integer
+        assoc.string(i)
+        assoc.integer
+        assoc.computed_string(i)
+        assoc.computed_integer
+        computed_assoc.string(i)
+        computed_assoc.integer
+        computed_assoc.computed_string(i)
+        computed_assoc.computed_integer
+      ]
+      all_possible_instructions = paths
+                                  .combination(2)
+                                  .flat_map do |path_1, path_2|
+        [:asc, 'desc']
+          .repeated_permutation(2)
+          .map do |pair|
+            Hash[[path_1, path_2].zip(pair)]
+          end
+      end
 
-            %w[asc desc].each do |direction|
-              context "when direction is #{direction.upcase}" do
-                context 'with default case-sensitive sort' do
-                  let(:sort_param) { { field => direction } }
-                  let(:expected_ids) do
-                    ActiveSet.new(Foo.all.to_a).sort(params[:sort]).map(&:id)
-                  end
+      all_possible_instructions.each do |instructions|
+        context "{ #{instructions} }" do
+          let(:instructions) { instructions }
 
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
-                end
+          it do
+            result.each_cons(2) do |left_result, right_result|
+              (path_1, path_2) = instructions.keys
+              (dir_1, dir_2) = instructions.values
 
-                context 'with case-INsensitive sort' do
-                  let(:sort_param) { { "#{field}(i)": direction } }
-                  let(:expected_ids) do
-                    ActiveSet.new(Foo.all.to_a).sort(params[:sort]).map(&:id)
-                  end
+              left_value_1 = value_for(path: path_1, object: left_result)
+              right_value_1 = value_for(path: path_1, object: right_result)
+              operator_1 = dir_1 == :asc ? '<=' : '>='
 
-                  it { expect(response).to have_http_status :ok }
-                  it { expect(results_ids).to eq expected_ids }
-                end
-              end
+              expect(left_value_1).to be.send(operator_1, right_value_1)
+
+              next unless left_value_1 == right_value_1
+
+              left_value_2 = value_for(path: path_2, object: left_result)
+              right_value_2 = value_for(path: path_2, object: right_result)
+              operator_2 = dir_2 == :asc ? '<=' : '>='
+
+              expect(left_value_2).to be.send(operator_2, right_value_2)
             end
           end
         end
