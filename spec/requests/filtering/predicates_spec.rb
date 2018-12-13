755e330cbe3af27d4157dcb76903c6495fda6771
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-12-13 13:38:00 -0500

    Refactor the predicates spec

--- spec/requests/filtering/predicates_spec.rb
+++ spec/requests/filtering/predicates_spec.rb
@@ -14,26 +14,6 @@ RSpec.describe 'GET /things?filter', type: :request do
   context '.json' do
     let(:results) { JSON.parse(response.body) }
     let(:result_ids) { results.map { |f| f['id'] } }
-    let(:other_thing) do
-      FactoryBot.build(:thing,
-                       boolean: !matching_item.boolean,
-                       only: FactoryBot.build(:only,
-                                              boolean: !matching_item.only.boolean))
-    end
-
-    def expected_result_ids(keypath, item, type, operator, value)
-      model_class = ActiveSet::AttributeInstruction.new(keypath, nil)
-                      .resource_for(item: item)
-                      .class
-      relation = model_class.where(
-        model_class.arel_table[type]
-                   .public_send(
-                     operator,
-                     value)
-      )
-
-      relation.pluck(:id)
-    end
 
     before(:each) do
       get things_path(format: :json),
@@ -42,17 +22,37 @@ RSpec.describe 'GET /things?filter', type: :request do
 
     (ApplicationRecord::FIELD_TYPES - %i[symbol bignum]).each do |type|
       [1, 2].each do |id|
-        let(:matching_item) { instance_variable_get("@thing_#{id}") }
-
-        # single value operators
+        # single value inclusive operators
         %i[
           eq
-          not_eq
-          lt
           lteq
-          gt
           gteq
           matches
+        ].each do |operator|
+          %W[
+            #{type}(#{operator})
+            only.#{type}(#{operator})
+          ].each do |path|
+            context "{ #{path}: }" do
+              let(:instruction_single_value) do
+                ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item)
+              end
+              let(:instructions) do
+                {
+                  path => instruction_single_value
+                }
+              end
+
+              it { expect(result_ids).to include matching_item.id }
+            end
+          end
+        end
+
+        # single value exlusive operators
+        %i[
+          not_eq
+          lt
+          gt
           does_not_match
         ].each do |operator|
           %W[
@@ -69,41 +69,35 @@ RSpec.describe 'GET /things?filter', type: :request do
                 }
               end
 
-              it { expect(result_ids).to eq expected_result_ids(path, matching_item, type, operator, instruction_single_value) }
+              it { expect(result_ids).not_to include matching_item.id }
             end
           end
         end
 
-        # multi value operators
+        # multi value inclusive operators
         %i[
           eq_any
           not_eq_any
-          eq_all
-          not_eq_all
           in
-          not_in
           in_any
           not_in_any
-          in_all
-          not_in_all
-          lt_any
           lteq_any
-          lt_all
-          lteq_all
-          gt_any
           gteq_any
-          gt_all
-          gteq_all
           matches_any
           does_not_match_any
-          matches_all
-          does_not_match_all
         ].each do |operator|
           %W[
             #{type}(#{operator})
             only.#{type}(#{operator})
           ].each do |path|
             context "{ #{path}: }" do
+              let(:matching_item) { instance_variable_get("@thing_#{id}") }
+              let(:other_thing) do
+                FactoryBot.build(:thing,
+                                 boolean: !matching_item.boolean,
+                                 only: FactoryBot.build(:only,
+                                                        boolean: !matching_item.only.boolean))
+              end
               let(:instruction_multi_value) do
                 [
                   ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
@@ -116,10 +110,90 @@ RSpec.describe 'GET /things?filter', type: :request do
                 }
               end
 
-              it { expect(result_ids).to eq expected_result_ids(path, matching_item, type, operator, instruction_multi_value) }
+              it { expect(result_ids).to include matching_item.id }
             end
           end
         end
+
+        # multi value exclusive operators
+        %i[
+          eq_all
+          not_eq_all
+          not_in
+          in_all
+          not_in_all
+          lt_all
+          gt_all
+          matches_all
+          does_not_match_all
+        ].each do |operator|
+          %W[
+            #{type}(#{operator})
+            only.#{type}(#{operator})
+          ].each do |path|
+            context "{ #{path}: }" do
+              let(:matching_item) { instance_variable_get("@thing_#{id}") }
+              let(:other_thing) do
+                FactoryBot.build(:thing,
+                                 boolean: !matching_item.boolean,
+                                 only: FactoryBot.build(:only,
+                                                        boolean: !matching_item.only.boolean))
+              end
+              let(:instruction_multi_value) do
+                [
+                  ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
+                  ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
+                ]
+              end
+              let(:instructions) do
+                {
+                  path => instruction_multi_value
+                }
+              end
+
+              it { expect(result_ids).not_to include matching_item.id }
+            end
+          end
+        end
+
+        # multi value mixed operators
+        # %i[
+        #   lt_any
+        #   lteq_all
+        #   gt_any
+        #   gteq_all
+        # ].each do |operator|
+        #   %W[
+        #     #{type}(#{operator})
+        #     only.#{type}(#{operator})
+        #   ].each do |path|
+        #     context "{ #{path}: }" do
+        #       let(:matching_item) { instance_variable_get("@thing_#{id}") }
+        #       let(:other_thing) do
+        #         FactoryBot.build(:thing,
+        #                          boolean: !matching_item.boolean,
+        #                          only: FactoryBot.build(:only,
+        #                                                 boolean: !matching_item.only.boolean))
+        #       end
+        #       let(:instruction_multi_value) do
+        #         [
+        #           ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
+        #           ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
+        #         ]
+        #       end
+        #       let(:instructions) do
+        #         {
+        #           path => instruction_multi_value
+        #         }
+        #       end
+
+        #       if type.presence_in(%i[binary datetime decimal float integer]) && operator == :gt_any
+        #         it { expect(result_ids).not_to include matching_item.id }
+        #       else
+        #       end
+        #     end
+        #   end
+        # end
       end
     end
   end
