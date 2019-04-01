e89e2c1afdc2b23a1fe52e0900ee2260df5ceb24
fractaledmind <stephen.margheim@gmail.com>
2019-04-01 09:58:40 +0200

    Make the date and datetime generators in the generic factory more resilient

--- spec/factories/generic.rb
+++ spec/factories/generic.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
+def fit_to_minmax(n, max:, min: 1)
+  max_diff = max * n.div(max)
+  fitted = n - max_diff
+
+  return fitted if fitted >= min
+  fit_to_minmax(fitted + 1, max: max, min: min)
+end
+
 FactoryBot.define do
   trait :generic do
     sequence(:binary) { |n| Base64.encode64 [Faker::Crypto.sha256, n].join('-') }
     sequence(:boolean, &:even?)
-    sequence(:date)     { |n| Faker::Date.between(n.send(:year).ago, n.send(:year).from_now).to_date }
-    sequence(:datetime) { |n| Faker::Date.between(n.send(:year).ago, n.send(:year).from_now).to_datetime }
+    sequence(:date) do |n|
+      [
+        "19#{rand(99).to_s.rjust(2, '0')}",
+        fit_to_minmax(n, max: 12).to_s.rjust(2, '0'),
+        fit_to_minmax(n, max: 31).to_s.rjust(2, '0')
+      ].join('-')
+    end
+    sequence(:datetime) do |n|
+      "#{date}T#{time}:#{fit_to_minmax(n, max: 60).to_s.rjust(2, '0')}+00:00"
+    end
     sequence(:decimal)  { |n| Faker::Number.decimal(2).to_f + n }
     sequence(:float)    { |n| Faker::Number.decimal(2).to_f + n }
     sequence(:integer, &:to_i)
@@ -13,6 +29,11 @@ FactoryBot.define do
       n.hash.abs.to_s.split('').map { |i| ('a'..'z').to_a.shuffle[i.to_i] }.join
     end
     sequence(:text)     { |n| [Faker::Lorem.paragraph, n].join('-') }
-    sequence(:time)     { |n| "#{n - (12 * n.div(12))}:#{rand(60)}" }
+    sequence(:time) do |n|
+      [
+        fit_to_minmax(n, max: 12).to_s.rjust(2, '0'),
+        rand(60).to_s.rjust(2, '0')
+      ].join(':')
+    end
   end
 end
