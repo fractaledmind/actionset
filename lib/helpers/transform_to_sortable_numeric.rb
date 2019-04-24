b1692990403ab63cfec0293197bf10bd9c796541
fractaledmind <stephen.margheim@gmail.com>
2019-04-24 16:21:01 +0200

    Fix the perf of the transform_to_sortable_numeric method

--- lib/helpers/transform_to_sortable_numeric.rb
+++ lib/helpers/transform_to_sortable_numeric.rb
@@ -15,19 +15,6 @@
 
 # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
 def transform_to_sortable_numeric(value)
-  def string_to_sortable_numeric(string)
-    string                                          # 'aB09ü'
-      .split('')                                    # ["a", "B", "0", "9", "ü"]
-      .map { |char| char.ord.to_s.rjust(3, '0') }   # ["097", "066", "048", "057", "252"]
-      .insert(1, '.')                               # ["097", ".", "066", "048", "057", "252"]
-      .reduce(&:concat)                             # "097.066048057252"
-      .to_r                                         # (24266512014313/250000000000)
-  end
-
-  def time_to_sortable_numeric(time)
-    # https://stackoverflow.com/a/30604935/2884386
-   (time.utc.to_f * 1000).round
-  end
   # https://www.justinweiss.com/articles/4-simple-memoization-patterns-in-ruby-and-one-gem/#and-what-about-parameters
   @sortable_numeric ||= Hash.new do |h, key|
     h[key] = if key.is_a?(Numeric)
@@ -50,3 +37,17 @@ def transform_to_sortable_numeric(value)
 end
 # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
 # rubocop:enable Style/AsciiComments
+
+def string_to_sortable_numeric(string)
+  string                                          # 'aB09ü'
+    .split('')                                    # ["a", "B", "0", "9", "ü"]
+    .map { |char| char.ord.to_s.rjust(3, '0') }   # ["097", "066", "048", "057", "252"]
+    .insert(1, '.')                               # ["097", ".", "066", "048", "057", "252"]
+    .reduce(&:concat)                             # "097.066048057252"
+    .to_r                                         # (24266512014313/250000000000)
+end
+
+def time_to_sortable_numeric(time)
+  # https://stackoverflow.com/a/30604935/2884386
+ (time.utc.to_f * 1000).round
+end
