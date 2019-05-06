25df69e7ac40180299bbeab25bc93ee8f343f886
fractaledmind <stephen.margheim@gmail.com>
2019-05-06 15:31:06 +0200

    Ensure that the export path helper is made available

--- lib/action_set/helpers/helper_methods.rb
+++ lib/action_set/helpers/helper_methods.rb
@@ -4,6 +4,7 @@ require_relative './sort/link_for_helper'
 require_relative './pagination/links_for_helper'
 require_relative './pagination/path_for_helper'
 require_relative './params/form_for_object_helper'
+require_relative './export/path_for_helper'
 
 module ActionSet
   module Helpers
@@ -11,6 +12,7 @@ module ActionSet
       include Sort::LinkForHelper
       include Pagination::LinksForHelper
       include Params::FormForObjectHelper
+      include Export::PathForHelper
     end
   end
 end
