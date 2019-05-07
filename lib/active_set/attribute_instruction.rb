5a0a18410b637421952e096993ea0fc4d5ea1944
fractaledmind <stephen.margheim@gmail.com>
2019-05-07 14:44:16 +0200

    Memoize the methods of AttributeInstructions so that we don't recompute them every time we access them

--- lib/active_set/attribute_instruction.rb
+++ lib/active_set/attribute_instruction.rb
@@ -18,35 +18,41 @@ class ActiveSet
     end
 
     def attribute
-      attribute = @keypath.last
-      return attribute.sub(operator_regex, '') if attribute&.match operator_regex
+      return @attribute if defined? @attribute
 
-      attribute
+      attribute_instruction = @keypath.last
+      @attribute = attribute_instruction&.sub(operator_regex, '')
     end
 
-    def operator(default: '==')
-      attribute = @keypath.last
-      return attribute[operator_regex, 1] if attribute&.match operator_regex
+    def operator
+      return @operator if defined? @operator
 
-      default
+      attribute_instruction = @keypath.last
+      @operator = attribute_instruction[operator_regex, 1]&.to_sym
     end
 
     def associations_array
+      return @associations_array if defined? @associations_array
       return [] unless @keypath.any?
 
-      @keypath.slice(0, @keypath.length - 1)
+      @associations_array = @keypath.slice(0, @keypath.length - 1)
     end
 
     def associations_hash
+      return @associations_hash if defined? @associations_hash
       return {} unless @keypath.any?
 
-      associations_array.reverse.reduce({}) do |hash, association|
+      @associations_hash = associations_array.reverse.reduce({}) do |hash, association|
         { association => hash }
       end
     end
 
     def value_for(item:)
-      resource_for(item: item).public_send(attribute)
+      @values_for ||= Hash.new do |h, key|
+        h[key] = resource_for(item: key).public_send(attribute)
+      end
+
+      @values_for[item]
     rescue StandardError
       # :nocov:
       nil
@@ -54,11 +60,15 @@ class ActiveSet
     end
 
     def resource_for(item:)
-      associations_array.reduce(item) do |resource, association|
-        return nil unless resource.respond_to? association
+      @resources_for ||= Hash.new do |h, key|
+        h[key] = associations_array.reduce(key) do |resource, association|
+          break nil unless resource.respond_to? association
 
-        resource.public_send(association)
+          resource.public_send(association)
+        end
       end
+
+      @resources_for[item]
     rescue StandardError
       # :nocov:
       nil
