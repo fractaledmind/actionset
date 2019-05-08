689bed283c4600587111ab75c59b176d21ff17cf
fractaledmind <stephen.margheim@gmail.com>
2019-05-08 15:06:13 +0200

    Refactor the filtering and sorting strategies to use the new set instruction classes

--- lib/active_set/filtering/enumerable_strategy.rb
+++ lib/active_set/filtering/enumerable_strategy.rb
@@ -1,77 +1,78 @@
 # frozen_string_literal: true
 
+require_relative '../enumerable_set_instruction'
+require 'active_support/core_ext/module/delegation'
+
 class ActiveSet
   module Filtering
     class EnumerableStrategy
+      delegate :attribute_instance,
+               :attribute_class,
+               :attribute_value,
+               :attribute_value_for,
+               :operator,
+               :attribute,
+        to: :@set_instruction
+
       def initialize(set, attribute_instruction)
         @set = set
         @attribute_instruction = attribute_instruction
+        @set_instruction = EnumerableSetInstruction.new(attribute_instruction, set)
       end
 
       def execute
         return false unless @set.respond_to? :select
 
-        @set.select do |item|
-          next attribute_matches_for?(item) if can_match_attribute_for?(item)
-          next class_method_matches_for?(item) if can_match_class_method_for?(item)
-
-          false
+        if execute_filter_operation?
+          set = filter_operation
+        elsif execute_intersect_operation?
+          begin
+            set = intersect_operation
+          rescue TypeError # thrown if intersecting with a non-Array
+            return false
+          end
+        else
+          return false
         end
+
+        set
       end
 
       private
 
-      def can_match_attribute_for?(item)
-        attribute_item = attribute_item_for(item)
-
-        return false unless attribute_item
-        return false unless attribute_item.respond_to?(@attribute_instruction.attribute)
-        return false if attribute_item.method(@attribute_instruction.attribute).arity.positive?
+      def execute_filter_operation?
+        return false unless attribute_instance
+        return false unless attribute_instance.respond_to?(attribute)
+        return false if attribute_instance.method(attribute).arity.positive?
 
         true
       end
 
-      def can_match_class_method_for?(item)
-        attribute_item = attribute_item_for(item)
-
-        return false unless attribute_item
-        return false unless attribute_item.class
-        return false unless attribute_item.class.respond_to?(@attribute_instruction.attribute)
-        return false if attribute_item.class.method(@attribute_instruction.attribute).arity.zero?
+      def execute_intersect_operation?
+        return false unless attribute_class
+        return false unless attribute_class.respond_to?(attribute)
+        return false if attribute_class.method(attribute).arity.zero?
 
         true
       end
 
-      def attribute_matches_for?(item)
-        @attribute_instruction
-          .value_for(item: item)
-          .public_send(
-            @attribute_instruction.operator,
-            @attribute_instruction.value
-          )
-      end
-
-      # rubocop:disable Metrics/MethodLength
-      def class_method_matches_for?(item)
-        maybe_item_or_collection_or_nil = attribute_item_for(item)
-                                          .class
-                                          .public_send(
-                                            @attribute_instruction.attribute,
-                                            @attribute_instruction.value
-                                          )
-        if maybe_item_or_collection_or_nil.nil?
-          false
-        elsif maybe_item_or_collection_or_nil.respond_to?(:each)
-          maybe_item_or_collection_or_nil.include? attribute_item_for(item)
-        else
-          maybe_item_or_collection_or_nil == attribute_item_for(item)
+      def filter_operation
+        @set.select do |item|
+          attribute_value_for(item)
+            .public_send(
+              operator,
+              attribute_value
+            )
         end
       end
-      # rubocop:enable Metrics/MethodLength
 
-      def attribute_item_for(item)
-        @attribute_instruction
-          .resource_for(item: item)
+      def intersect_operation
+        other_set = attribute_class
+          .public_send(
+            attribute,
+            attribute_value
+          )
+        @set & other_set
       end
     end
   end
