6d0ecb0b7290f9f5478a07821fdabb9ca0f359d6
fractaledmind <stephen.margheim@gmail.com>
2019-05-11 14:58:13 +0200

    Update the comment docs for the flatten_keys_of helper method

--- lib/helpers/flatten_keys_of.rb
+++ lib/helpers/flatten_keys_of.rb
@@ -5,16 +5,16 @@ require 'active_support/core_ext/array/wrap'
 # Returns a flat hash where all nested keys are collapsed into an array of keys.
 #
 #   hash = { person: { name: { first: 'Rob' }, age: '28' } }
-#   hash.flatten_keys_to_array
-#   => {[:person, :name, :first] => "Rob", [:person, :age]=>"28" }
+#   flatten_keys_of(hash)
+#   => { [:person, :name, :first]=>"Rob", [:person, :age]=>"28" }
 #
 # Can also pass a Proc to change how nested keys are collapsed:
-#   flatten_keys_of(hash, ->(*keys) { keys.join('.') })
-#   => { 'person.name.first' => "Rob", 'person.age'=>"28" }
-#   flatten_keys_of(self, ->(*keys) { keys.join('-') })
-#   => { 'person-name-first' => "Rob", 'person-age'=>"28" }
-#   flatten_keys_of(self, ->(*keys) { keys.map(&:to_s).reduce { |memo, key| memo + "[#{key}]" } })
-#   => { 'person[name][first]' => "Rob", 'person[age]'=>"28" }
+#   flatten_keys_of(hash, flattener: ->(*keys) { keys.join('.') })
+#   => { "person.name.first"=>"Rob", "person.age"=>"28" }
+#   flatten_keys_of(hash, flattener: ->(*keys) { keys.join('-') })
+#   => { "person-name-first"=>"Rob", "person-age"=>"28" }
+#   flatten_keys_of(hash, flattener: ->(*keys) { keys.map(&:to_s).reduce { |memo, key| memo + "[#{key}]" } })
+#   => { "person[name][first]"=>"Rob", "person[age]"=>"28" }
 
 # refactored from https://stackoverflow.com/a/23861946/2884386
 def flatten_keys_of(input, keys = [], output = {}, flattener: ->(*keys) { keys }, flatten_arrays: false)
