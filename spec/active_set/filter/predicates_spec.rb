55144bcd4de4efefaa8f400cf1e8187874b59d77
fractaledmind <stephen.margheim@gmail.com>
2019-06-29 20:38:54 +0200

    Update the filtering specs
    Test both ActiveRecord and Enumerable sets
    Fix the scope spec when dealing with computed scopes
    Make the predicate specs work off of the filtering predicate constants

--- spec/active_set/filter/predicates_spec.rb
+++ spec/active_set/filter/predicates_spec.rb
@@ -2,189 +2,197 @@
 
 require 'spec_helper'
 
+# PREDICATE_OPERATORS = ActiveSet::Filtering::ActiveRecord::Constants::PREDICATE_OPERATORS
+# INCLUSIVE_UNARY_OPERATORS = PREDICATE_OPERATORS
+#   .select do |o|
+#     o[:type] == :unary &&
+#     o[:matching_behavior] == :inclusive
+#   end
+# EXCLUSIVE_UNARY_OPERATORS = PREDICATE_OPERATORS
+#   .select do |o|
+#     o[:type] == :unary &&
+#     o[:matching_behavior] == :exclusive
+#   end
+# INCONCLUSIVE_UNARY_OPERATORS = PREDICATE_OPERATORS
+#   .select do |o|
+#     o[:type] == :unary &&
+#     o[:matching_behavior] == :inconclusive
+#   end
+
+
 RSpec.describe ActiveSet do
+  PREDICATE_OPERATORS = ActiveSet::Filtering::Constants::BASE_PREDICATES
+  INCLUSIVE_UNARY_OPERATORS = PREDICATE_OPERATORS.select do |_, o|
+    o[:compound] == false &&
+    o[:behavior] == :inclusive
+  end.map(&:first)
+  EXCLUSIVE_UNARY_OPERATORS = PREDICATE_OPERATORS.select do |_, o|
+    o[:compound] == false &&
+    o[:behavior] == :exclusive
+  end.map(&:first)
+  INCLUSIVE_BINARY_OPERATORS = PREDICATE_OPERATORS.select do |_, o|
+    o[:compound] == true &&
+    o[:behavior] == :inclusive
+  end.map(&:first)
+  EXCLUSIVE_BINARY_OPERATORS = PREDICATE_OPERATORS.select do |_, o|
+    o[:compound] == true &&
+    o[:behavior] == :exclusive
+  end.map(&:first)
+
   before(:all) do
     @thing_1 = FactoryBot.create(:thing)
     @thing_2 = FactoryBot.create(:thing)
-    @active_set = ActiveSet.new(Thing.all)
   end
 
   describe '#filter' do
-    let(:results) { @active_set.filter(instructions) }
-    let(:result_ids) { results.map(&:id) }
+    ['itself', 'to_a'].each do |relation_mutator|
+      context "/#{ relation_mutator == 'itself' ? 'ActiveRecord' : 'Enumberable' }/" do
+        before(:all) do
+          @active_set = ActiveSet.new(Thing.all.public_send(relation_mutator))
+        end
+        let(:results) { @active_set.filter(instructions) }
+        let(:result_ids) { results.map(&:id) }
 
-    ApplicationRecord::DB_FIELD_TYPES.each do |type|
-      [1, 2].each do |id|
-        # single value inclusive operators
-        %i[
-          eq
-          lteq
-          gteq
-          matches
-        ].each do |operator|
-          %W[
-            #{type}(#{operator})
-            only.#{type}(#{operator})
-          ].each do |path|
-            context "{ #{path}: }" do
-              let(:matching_item) { instance_variable_get("@thing_#{id}") }
-              let(:instruction_single_value) do
-                ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item)
-              end
-              let(:instructions) do
-                {
-                  path => instruction_single_value
-                }
-              end
+        ApplicationRecord::DB_FIELD_TYPES.each do |type|
+          [1, 2].each do |id|
+            INCLUSIVE_UNARY_OPERATORS.each do |operator|
+              %W[
+                #{type}(#{operator})
+                only.#{type}(#{operator})
+              ].each do |path|
+                context "{ #{path}: }" do
+                  let(:matching_item) { instance_variable_get("@thing_#{id}") }
+                  let(:instruction_single_value) do
+                    ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item)
+                  end
+                  let(:instructions) do
+                    {
+                      path => instruction_single_value
+                    }
+                  end
 
-              it { expect(result_ids).to include matching_item.id }
+                  it { expect(result_ids).to include matching_item.id }
+                end
+              end
             end
+
+            EXCLUSIVE_UNARY_OPERATORS.each do |operator|
+              %W[
+                #{type}(#{operator})
+                only.#{type}(#{operator})
+              ].each do |path|
+                context "{ #{path}: }" do
+                  let(:matching_item) { instance_variable_get("@thing_#{id}") }
+                  let(:instruction_single_value) do
+                    ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item)
+                  end
+                  let(:instructions) do
+                    {
+                      path => instruction_single_value
+                    }
+                  end
+
+                  it { expect(result_ids).not_to include matching_item.id }
+                end
+              end
+            end
+
+            INCLUSIVE_BINARY_OPERATORS.each do |operator|
+              %W[
+                #{type}(#{operator})
+                only.#{type}(#{operator})
+              ].each do |path|
+                context "{ #{path}: }" do
+                  let(:matching_item) { instance_variable_get("@thing_#{id}") }
+                  let(:other_thing) do
+                    guaranteed_unique_object_for(matching_item,
+                                                 only: guaranteed_unique_object_for(matching_item.only))
+                  end
+                  let(:instruction_multi_value) do
+                    [
+                      ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
+                      ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
+                    ]
+                  end
+                  let(:instructions) do
+                    {
+                      path => instruction_multi_value
+                    }
+                  end
+
+                  it { expect(result_ids).to include matching_item.id }
+                end
+              end
+            end
+
+            EXCLUSIVE_BINARY_OPERATORS.each do |operator|
+              %W[
+                #{type}(#{operator})
+                only.#{type}(#{operator})
+              ].each do |path|
+                context "{ #{path}: }" do
+                  let(:matching_item) { instance_variable_get("@thing_#{id}") }
+                  let(:other_thing) do
+                    guaranteed_unique_object_for(matching_item,
+                                                 only: guaranteed_unique_object_for(matching_item.only))
+                  end
+                  let(:instruction_multi_value) do
+                    [
+                      ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
+                      ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
+                    ]
+                  end
+                  let(:instructions) do
+                    {
+                      path => instruction_multi_value
+                    }
+                  end
+
+                  it { expect(result_ids).not_to include matching_item.id }
+                end
+              end
+            end
+
+            # multi value mixed operators
+            # %i[
+            #   lt_any
+            #   lteq_all
+            #   gt_any
+            #   gteq_all
+            # ].each do |operator|
+            #   %W[
+            #     #{type}(#{operator})
+            #     only.#{type}(#{operator})
+            #   ].each do |path|
+            #     context "{ #{path}: }" do
+            #       let(:matching_item) { instance_variable_get("@thing_#{id}") }
+            #       let(:other_thing) do
+            #         FactoryBot.build(:thing,
+            #                          boolean: !matching_item.boolean,
+            #                          only: FactoryBot.build(:only,
+            #                                                 boolean: !matching_item.only.boolean))
+            #       end
+            #       let(:instruction_multi_value) do
+            #         [
+            #           ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
+            #           ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
+            #         ]
+            #       end
+            #       let(:instructions) do
+            #         {
+            #           path => instruction_multi_value
+            #         }
+            #       end
+
+            #       if type.presence_in(%i[binary datetime decimal float integer]) && operator == :gt_any
+            #         it { expect(result_ids).not_to include matching_item.id }
+            #       else
+            #       end
+            #     end
+            #   end
+            # end
           end
         end
-
-        # single value exlusive operators
-        %i[
-          not_eq
-          lt
-          gt
-          does_not_match
-        ].each do |operator|
-          %W[
-            #{type}(#{operator})
-            only.#{type}(#{operator})
-          ].each do |path|
-            context "{ #{path}: }" do
-              let(:matching_item) { instance_variable_get("@thing_#{id}") }
-              let(:instruction_single_value) do
-                ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item)
-              end
-              let(:instructions) do
-                {
-                  path => instruction_single_value
-                }
-              end
-
-              it { expect(result_ids).not_to include matching_item.id }
-            end
-          end
-        end
-
-        # multi value inclusive operators
-        %i[
-          eq_any
-          not_eq_any
-          in
-          in_any
-          not_in_any
-          lteq_any
-          gteq_any
-          matches_any
-          does_not_match_any
-        ].each do |operator|
-          %W[
-            #{type}(#{operator})
-            only.#{type}(#{operator})
-          ].each do |path|
-            context "{ #{path}: }" do
-              let(:matching_item) { instance_variable_get("@thing_#{id}") }
-              let(:other_thing) do
-                guaranteed_unique_object_for(matching_item,
-                                             only: guaranteed_unique_object_for(matching_item.only))
-              end
-              let(:instruction_multi_value) do
-                [
-                  ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
-                  ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
-                ]
-              end
-              let(:instructions) do
-                {
-                  path => instruction_multi_value
-                }
-              end
-
-              it { expect(result_ids).to include matching_item.id }
-            end
-          end
-        end
-
-        # multi value exclusive operators
-        %i[
-          eq_all
-          not_eq_all
-          not_in
-          in_all
-          not_in_all
-          lt_all
-          gt_all
-          matches_all
-          does_not_match_all
-        ].each do |operator|
-          %W[
-            #{type}(#{operator})
-            only.#{type}(#{operator})
-          ].each do |path|
-            context "{ #{path}: }" do
-              let(:matching_item) { instance_variable_get("@thing_#{id}") }
-              let(:other_thing) do
-                guaranteed_unique_object_for(matching_item,
-                                             only: guaranteed_unique_object_for(matching_item.only))
-              end
-              let(:instruction_multi_value) do
-                [
-                  ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
-                  ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
-                ]
-              end
-              let(:instructions) do
-                {
-                  path => instruction_multi_value
-                }
-              end
-
-              it { expect(result_ids).not_to include matching_item.id }
-            end
-          end
-        end
-
-        # multi value mixed operators
-        # %i[
-        #   lt_any
-        #   lteq_all
-        #   gt_any
-        #   gteq_all
-        # ].each do |operator|
-        #   %W[
-        #     #{type}(#{operator})
-        #     only.#{type}(#{operator})
-        #   ].each do |path|
-        #     context "{ #{path}: }" do
-        #       let(:matching_item) { instance_variable_get("@thing_#{id}") }
-        #       let(:other_thing) do
-        #         FactoryBot.build(:thing,
-        #                          boolean: !matching_item.boolean,
-        #                          only: FactoryBot.build(:only,
-        #                                                 boolean: !matching_item.only.boolean))
-        #       end
-        #       let(:instruction_multi_value) do
-        #         [
-        #           ActiveSet::AttributeInstruction.new(path, nil).value_for(item: matching_item),
-        #           ActiveSet::AttributeInstruction.new(path, nil).value_for(item: other_thing)
-        #         ]
-        #       end
-        #       let(:instructions) do
-        #         {
-        #           path => instruction_multi_value
-        #         }
-        #       end
-
-        #       if type.presence_in(%i[binary datetime decimal float integer]) && operator == :gt_any
-        #         it { expect(result_ids).not_to include matching_item.id }
-        #       else
-        #       end
-        #     end
-        #   end
-        # end
       end
     end
   end
--- spec/active_set/filter/scopes_spec.rb
+++ spec/active_set/filter/scopes_spec.rb
@@ -6,32 +6,34 @@ RSpec.describe ActiveSet do
   before(:all) do
     @thing_1 = FactoryBot.create(:thing)
     @thing_2 = FactoryBot.create(:thing)
-    @active_set = ActiveSet.new(Thing.all)
   end
 
   describe '#filter' do
-    let(:result) { @active_set.filter(instructions) }
+    ['itself', 'to_a'].each do |relation_mutator|
+      context "/#{ relation_mutator == 'itself' ? 'ActiveRecord' : 'Enumberable' }/" do
+        before(:all) do
+          @active_set = ActiveSet.new(Thing.all.public_send(relation_mutator))
+        end
+        let(:result) { @active_set.filter(instructions) }
 
-    ApplicationRecord::DB_FIELD_TYPES.each do |type|
-      [1, 2].each do |id|
-        let(:matching_item) { instance_variable_get("@thing_#{id}") }
+        ApplicationRecord::DB_FIELD_TYPES.each do |type|
+          [1, 2].each do |id|
+            let(:matching_item) { instance_variable_get("@thing_#{id}") }
 
-        all_possible_scope_paths_for(type).each do |path|
-          context "{ #{path}: }" do
-            let(:instructions) do
-              {
-                path => filter_value_for(object: matching_item, path: path)
-              }
-            end
+            all_possible_scope_paths_for(type).each do |path|
+              context "{ #{path}: }" do
+                let(:instructions) do
+                  {
+                    path => filter_value_for(object: matching_item, path: path)
+                  }
+                end
 
-            if path.end_with?('_collection_method', '_scope_method')
-              if path.include?('computed_')
-                it { expect(result.map(&:id)).to eq [] }
-              else
-                it { expect(result.map(&:id)).to eq [matching_item.id] }
+                if path.end_with?('_collection_method', '_scope_method')
+                  it { expect(result.map(&:id)).to eq [matching_item.id] }
+                else
+                  it { expect(result.map(&:id)).to eq Thing.pluck(:id) }
+                end
               end
-            else
-              it { expect(result.map(&:id)).to eq Thing.pluck(:id) }
             end
           end
         end
