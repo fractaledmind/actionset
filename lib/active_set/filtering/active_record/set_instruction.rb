4c22c19f93ec4cdab22a5815b30e34080060dcd8
fractaledmind <stephen.margheim@gmail.com>
2019-06-30 08:12:19 +0200

    Update the various SetInstruction classes to work with all of the predicates now

--- lib/active_set/filtering/active_record/set_instruction.rb
+++ lib/active_set/filtering/active_record/set_instruction.rb
@@ -8,20 +8,42 @@ class ActiveSet
     module ActiveRecord
       class SetInstruction < ActiveRecordSetInstruction
         def arel_operator
-          instruction_operator = @attribute_instruction.operator
-          return :eq unless instruction_operator
-
-          operator_hash = Operators.get(instruction_operator)
-          operator_hash&.dig(:operator)
+          operator_hash.fetch(:operator, :eq)
         end
 
         def arel_value
           return @arel_value if defined? @arel_value
 
-          arel_value = @attribute_instruction.value
+          arel_value = guarantee_attribute_type(@attribute_instruction.value)
+          arel_value = query_attribute_for(arel_value)
           arel_value = arel_value.downcase if case_insensitive_operation?
 
           @arel_value = arel_value
+        end
+
+        private
+
+        def query_attribute_for(value)
+          return value unless operator_hash.key?(:query_attribute_transformer)
+
+          operator_hash[:query_attribute_transformer].call(value)
+        end
+
+        def operator_hash
+          instruction_operator = @attribute_instruction.operator
+
+          Operators.get(instruction_operator)
+        end
+
+        def guarantee_attribute_type(attribute)
+          # Booleans don't respond to many operator methods,
+          # so we cast them to however the database expects them
+          return attribute if not arel_type == :boolean
+          return attribute.map { |a| guarantee_attribute_type(a) } if attribute.respond_to?(:each)
+
+          sql_value = Arel::Nodes::Casted.new(attribute, arel_column).to_sql
+          sql_value = sql_value[/'(.*?)'/, 1] if sql_value.match? /'.*?'/
+          sql_value
         end
       end
     end
--- lib/active_set/filtering/enumerable/set_instruction.rb
+++ lib/active_set/filtering/enumerable/set_instruction.rb
@@ -8,11 +8,50 @@ class ActiveSet
     module Enumerable
       class SetInstruction < EnumerableSetInstruction
         def item_matches_query?(item)
-          result = object_attribute_for(item)
-            .public_send(
-              operator_method,
-              query_attribute
-            )
+          return query_result_for(item, query_attribute_for(instruction_value)) if not operator_hash.key?(:reducer)
+
+          instruction_value.public_send(operator_hash[:reducer]) do |value|
+            query_result_for(item, query_attribute_for(value))
+          end
+        end
+
+        def set_item
+          return @set_item if defined? @set_item
+
+          @set_item = @set.find(&:present?)
+        end
+
+        def attribute_instance
+          return set_item if @attribute_instruction.associations_array.empty?
+          return @attribute_model if defined? @attribute_model
+
+          @attribute_model = @attribute_instruction
+                             .associations_array
+                             .reduce(set_item) do |obj, assoc|
+            obj.public_send(assoc)
+          end
+        end
+
+        def attribute_class
+          attribute_instance&.class
+        end
+
+        private
+
+        def query_result_for(item, value)
+          result = if operator_method == :cover? && value.is_a?(Range)
+            value
+              .public_send(
+                operator_method,
+                object_attribute_for(item)
+              )
+          else
+            object_attribute_for(item)
+              .public_send(
+                operator_method,
+                value
+              )
+          end
 
           return result unless operator_hash.key?(:result_transformer)
 
@@ -26,8 +65,8 @@ class ActiveSet
           operator_hash[:object_attribute_transformer].call(attribute)
         end
 
-        def query_attribute
-          attribute = guarantee_attribute_type(instruction_value)
+        def query_attribute_for(value)
+          attribute = guarantee_attribute_type(value)
           return attribute unless operator_hash.key?(:query_attribute_transformer)
 
           operator_hash[:query_attribute_transformer].call(attribute)
@@ -51,27 +90,6 @@ class ActiveSet
           return attribute.map { |a| guarantee_attribute_type(a) } if attribute.respond_to?(:each)
 
           attribute
-        end
-
-        def set_item
-          return @set_item if defined? @set_item
-
-          @set_item = @set.find(&:present?)
-        end
-
-        def attribute_instance
-          return set_item if @attribute_instruction.associations_array.empty?
-          return @attribute_model if defined? @attribute_model
-
-          @attribute_model = @attribute_instruction
-                             .associations_array
-                             .reduce(set_item) do |obj, assoc|
-            obj.public_send(assoc)
-          end
-        end
-
-        def attribute_class
-          attribute_instance&.class
         end
       end
     end
