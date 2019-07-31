3920a4fa5de6db0e26d85629a87b9fa8a034da0b
fractaledmind <stephen.margheim@gmail.com>
2019-07-31 09:55:36 +0200

    Add view helpers for the range of records shown on the current page

--- lib/action_set/helpers/pagination/total_pages_for_helper.rb
+++ lib/action_set/helpers/pagination/total_pages_for_helper.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require_relative './record_size_for_helper'
 require_relative './page_size_for_helper'
 
 module Pagination
   module TotalPagesForHelper
+    include Pagination::RecordSizeForHelper
     include Pagination::PageSizeForHelper
 
     def pagination_total_pages_for(set)
-      total_set_size = set.instructions.dig(:paginate, :count)
+      total_set_size = pagination_record_size_for(set)
       return 1 if total_set_size.zero?
 
       (total_set_size.to_f / pagination_page_size_for(set)).ceil
