7b709b96cb45b8f0eb53dbdf964195056a756f5c
fractaledmind <stephen.margheim@gmail.com>
2019-08-02 14:20:29 +0200

    Add specs (with some fixes) for the new pagination record helpers

--- lib/action_set/helpers/pagination/record_range_for_helper.rb
+++ lib/action_set/helpers/pagination/record_range_for_helper.rb
@@ -5,10 +5,16 @@ require_relative './record_last_for_helper'
 
 module Pagination
   module RecordRangeForHelper
+    include Pagination::CurrentPageForHelper
+    include Pagination::TotalPagesForHelper
     include Pagination::RecordFirstForHelper
     include Pagination::RecordLastForHelper
 
     def pagination_record_range_for(set)
+      current_page = pagination_current_page_for(set)
+      total_pages = pagination_total_pages_for(set)
+      return 'None' if current_page > total_pages
+
       [
         pagination_record_first_for(set),
         '&ndash;',
