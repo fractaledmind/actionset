e00d4a31cb4ce3afa836f31d9b4e7885151c25e2
fractaledmind <stephen.margheim@gmail.com>
2020-03-26 15:09:00 +0100

    Fix the `guaranteed_unique_attributes_for` helper method to handle date/time objects safely

--- spec/support/factory_helpers.rb
+++ spec/support/factory_helpers.rb
@@ -13,14 +13,14 @@ module FactoryHelpers
     {}.tap do |h|
       h[:binary] = guaranteed_unique_string_for(obj.binary)
       h[:boolean] = !obj.boolean
-      h[:date] = obj.date.next_day
-      h[:datetime] = obj.datetime.next_day
+      h[:date] = obj.date.to_date.next_day
+      h[:datetime] = obj.datetime.to_datetime.next_day
       h[:decimal] = obj.decimal + 1
       h[:float] = obj.float + 1
       h[:integer] = obj.integer + 1
       h[:string] = guaranteed_unique_string_for(obj.string)
       h[:text] = obj.text.split.map { |w| guaranteed_unique_string_for(w) }.join(' ')
-      h[:time] = obj.time.advance(hours: 1)
+      h[:time] = obj.time.to_time.advance(hours: 1)
     end
   end
 
