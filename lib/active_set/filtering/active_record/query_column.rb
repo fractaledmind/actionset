cafbaa3415fc70e689d81f631931e77afc5f52c2
fractaledmind <stephen.margheim@gmail.com>
2020-03-26 15:10:36 +0100

    MySQL can't match against float properly, so cast to decimals
    https://dev.mysql.com/doc/refman/8.0/en/problems-with-float.html

--- lib/active_set/filtering/active_record/query_column.rb
+++ lib/active_set/filtering/active_record/query_column.rb
@@ -9,6 +9,8 @@ class ActiveSet
 
           @query_column = if must_cast_numerical_column?
                             column_cast_as_char
+                          elsif must_cast_mysql_float_column?
+                            column_cast_as_decimal
                           else
                             arel_column
                           end
@@ -21,6 +23,16 @@ class ActiveSet
           # NOTE: this is can be quite inefficient, as it forces the DB engine to perform that cast on all rows.
           # https://www.ryadel.com/en/like-operator-equivalent-integer-numeric-columns-sql-t-sql-database/
           Arel::Nodes::NamedFunction.new('CAST', [arel_column.as('CHAR')])
+        end
+
+        def column_cast_as_decimal
+          Arel::Nodes::NamedFunction.new('CAST', [arel_column.as('DECIMAL(8,2)')])
+        end
+
+        def must_cast_mysql_float_column?
+          return false unless ::ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+
+          arel_type == :float
         end
 
         def must_cast_numerical_column?
