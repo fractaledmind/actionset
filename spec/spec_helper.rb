9160cc4003699eb419d2c17db0bea773b25ea1c9
fractaledmind <stephen.margheim@gmail.com>
2020-04-18 18:14:15 +0200

    Silence unhelpful deprecation warning about type casting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,17 @@ database.connect
 
 Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
+# "DEPRECATION WARNING: You attempted to assign a value which is not explicitly `true` or `false` () to a boolean column. Currently this value casts to `false`. This will change to match Ruby's semantics, and will cast to `true` in Rails 5. If you would like to maintain the current behavior, you should explicitly handle the values you would like cast to `false`. (called from typecast at /Users/margheim/Dropbox/gems/actionset/lib/action_set/attribute_value.rb:98)
+deprecation_warnings_to_silence = [
+  /attempted to assign a value which is not explicitly `true` or `false`/,
+]
+
+ActiveSupport::Deprecation.behavior = lambda do |message, callstack|
+  unless message =~ Regexp.new(deprecation_warnings_to_silence.join('|'))
+    ActiveSupport::Deprecation::DEFAULT_BEHAVIORS[:stderr].call(message, callstack)
+  end
+end
+
 RSpec.configure do |config|
   include PathHelpers
   include FilteringHelpers
